395a4b788bd4867cf49b3e68f2ce5c3c774c7b37
Chris Häußler (@ Windows) <chrissx@chrissx.de>
2021-01-11 19:55:38 +0100

    delta can be simpler

--- src/hs/delta.clj
+++ src/hs/delta.clj
@@ -1,9 +1,6 @@
 (ns hs.delta
   (:gen-class))
 
-(defn file-unchanged [k d1 d2]
-  (& (contains? d1 k) (contains? d2 k) (= (get d1 k) (get d2 k))))
-
 (defn delta [d1 d2]
   "Gives you all of the files in d2 that were changed since d1."
-  (keys (dissoc d2 (find (fn [k] (file-unchanged k d1 d2)) (keys d2)))))
+  (remove (fn [k] (& (contains? d1 k) (= (get d1 k) (get d2 k)))) (keys d2)))
